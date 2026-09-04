2bc4ab218263163493fe1237297b79497f9e3eb9
gustavobachiao <bachiaogustavo.faria@gmail.com>
2026-09-04 00:13:52 -0300

    Update Solar-copiar-nome-partes.user.js
    Updated author information to include Unidade Passos.

--- Solar-copiar-nome-partes.user.js
+++ Solar-copiar-nome-partes.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://solar.defensoria.mg.def.br/
 // @version      1.0.0
 // @description  Adiciona botao de copia ao lado do nome das partes na tela inicial e nos detalhes do processo.
-// @author       Defensoria Publica de Minas Gerais
+// @author       Defensoria Publica de Minas Gerais - Unidade Passos
 // @match        https://solar.defensoria.mg.def.br/*
 // @grant        none
 // @run-at       document-idle
