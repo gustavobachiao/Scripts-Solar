5ccb63ecdb67c59030793c3b2332fc8d2fb15b2b
gustavobachiao <bachiaogustavo.faria@gmail.com>
2026-09-03 23:03:18 -0300

    Update Solar-busca-linha-do-tempo.user.js
    Updated URLs to point to the new repository for the script.

--- Solar-busca-linha-do-tempo.user.js
+++ Solar-busca-linha-do-tempo.user.js
@@ -7,10 +7,10 @@
 // @match        https://solar.defensoria.mg.def.br/*
 // @grant        none
 // @run-at       document-idle
-// @updateURL    https://raw.githubusercontent.com/SEU-USUARIO/Scripts-Solar/main/solar-busca-linha-do-tempo.user.js
-// @downloadURL  https://raw.githubusercontent.com/SEU-USUARIO/Scripts-Solar/main/solar-busca-linha-do-tempo.user.js
-// @homepageURL  https://github.com/SEU-USUARIO/Scripts-Solar
-// @supportURL   https://github.com/SEU-USUARIO/Scripts-Solar/issues
+// @updateURL    https://raw.githubusercontent.com/gustavobachiao/Scripts-Solar/main/Solar-busca-linha-do-tempo.user.js
+// @downloadURL  https://raw.githubusercontent.com/gustavobachiao/Scripts-Solar/main/Solar-busca-linha-do-tempo.user.js
+// @homepageURL  https://github.com/gustavobachiao/Scripts-Solar
+// @supportURL   https://github.com/gustavobachiao/Scripts-Solar/issues
 // ==/UserScript==
 
 (function() {
