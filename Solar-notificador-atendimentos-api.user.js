0df45345b3431693183010e5e6e1d54d1fa9714e
gustavobachiao <bachiaogustavo.faria@gmail.com>
2026-09-09 13:06:16 -0300

    Update Solar-notificador-atendimentos-api.user.js

--- Solar-notificador-atendimentos-api.user.js
+++ Solar-notificador-atendimentos-api.user.js
@@ -12,7 +12,7 @@
 // @grant        GM_addStyle
 // @grant        GM_registerMenuCommand
 // @run-at       document-idle
-// @updateURL    https://raw.githubusercontent.com/Scripts-Solar/main/Solar-notificador-atendimentos-api.user.js
+// @updateURL    https://raw.githubusercontent.com/gustavobachiao/Scripts-Solar/main/Solar-notificador-atendimentos-api.user.js
 // @downloadURL  https://raw.githubusercontent.com/Scripts-Solar/main/Solar-notificador-atendimentos-api.user.js
 // @homepageURL  https://github.com/gustavobachiao/Scripts-Solar
 // @supportURL   https://github.com/gustavobachiao/Scripts-Solar/issues
